108cd5106c67dd0c7931695af27569802aab948c
Fabjon Hoxhaj <fahox1991@gmail.com>
2022-05-24 00:31:54 +0200

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { firebaseConfig } from '../environments/environment';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
